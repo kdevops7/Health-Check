ee97a921337c266280c519ce6c819f36aadfdb3b
Kauan Bazoti <denis.blinders@gmail.com>
2025-05-13 21:20:34 -0300

    feat: adiciona status OK ao script

--- health_check.py
+++ health_check.py
@@ -1,4 +1,7 @@
-print("Health check script iniciado com sucesso.")
+print("Iniciando health check com sucesso!")
+print("Status: OK")
+
+## print("Health check script iniciado com sucesso.") - print de início
 
 
 ## Esse script é só um placeholder (espaço reservado). Depois podemos colocar um health check real aqui.
